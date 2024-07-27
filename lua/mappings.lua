17b0efed51b33a35fe95375d22acc5bf4eec8755
hbinhng <me@hbinhng.dev>
2024-07-27 09:02:57 +0000

    Add create terminal in new tab shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,4 +10,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "<C-G>", cmp.mapping.complete())
 
+map("n", "<leader>tt", function()
+  vim.cmd ":tab term"
+end, {
+  desc = "Quick open terminal in new tab",
+  noremap = true,
+  silent = true,
+})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
